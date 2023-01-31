d7bf0e947571ecda0163442b0b57f3ace2fa3372
Andrey (dronn1k) Vakarchuk <dronn1k.dev@gmail.com>
2023-01-31 09:30:25 +0200

    v0.0.1

---
+++ lib/observer.dart
@@ -0,0 +1,50 @@
+library reactive_variables;
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'variable.dart';
+
+class Obs extends StatefulWidget {
+  final List<Rv> rvList;
+  final Widget Function() builder;
+
+  const Obs({
+    Key? key,
+    required this.rvList,
+    required this.builder,
+  }) : super(key: key);
+
+  @override
+  State<Obs> createState() => _ObsState();
+}
+
+class _ObsState extends State<Obs> {
+  final List<StreamSubscription> _subList = [];
+
+  @override
+  void initState() {
+    for (var element in widget.rvList) {
+      _subList.add(element.listen((value) {
+        if (mounted) {
+          setState(() {});
+        }
+      }));
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    for (var element in _subList) {
+      element.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.builder.call();
+  }
+}
---
+++ lib/variable.dart
@@ -0,0 +1,45 @@
+library reactive_variables;
+
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+
+class Rv<T> extends ChangeNotifier implements ValueListenable {
+  T _value;
+  @override
+  T get value => _value;
+  set value(T newValue) {
+    if (_value == newValue) return;
+    _value = newValue;
+    _valueStreamCtrl.add(_value);
+    notifyListeners();
+  }
+
+  final StreamController<T> _valueStreamCtrl = StreamController.broadcast();
+
+  Rv(this._value);
+
+  void trigger(T v) {
+    _value = v;
+    _valueStreamCtrl.add(_value);
+    notifyListeners();
+  }
+
+  void refresh() {
+    _valueStreamCtrl.add(value);
+    notifyListeners();
+  }
+
+  StreamSubscription<T> listen(
+    void Function(T value) listener, {
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) =>
+      _valueStreamCtrl.stream.listen(
+        listener,
+        onError: onError,
+        onDone: onDone,
+        cancelOnError: cancelOnError,
+      );
+}
