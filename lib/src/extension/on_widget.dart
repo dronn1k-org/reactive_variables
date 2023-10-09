9a8260c8a6f0667be62e100b2b9de863d810d8bb
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2023-10-09 15:36:37 +0300

    OnWidget code fix.

--- lib/src/extension/on_widget.dart
+++ lib/src/extension/on_widget.dart
@@ -3,5 +3,5 @@ import 'package:reactive_variables/reactive_variables.dart';
 
 extension OnWidget on Widget {
   Obs observe(List<Rv<dynamic>> rvList) =>
-      Obs(rvList: rvList, builder: () => this);
+      Obs(rvList: rvList, builder: (context) => this);
 }
