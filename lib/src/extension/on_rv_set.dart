c848cf8e19570801f8563e0a0392d001d8c7947b
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2023-10-09 13:03:06 +0300

    OnRvSet extension has been implemented.

---
+++ lib/src/extension/on_rv_set.dart
@@ -0,0 +1,70 @@
+part of '../variable.dart';
+
+extension OnRvSet<T> on Rv<Set<T>> {
+  T operator [](int index) {
+    return value.elementAt(index);
+  }
+
+  void operator []=(int index, T value) {
+    this[index] = value;
+    refresh();
+  }
+
+  bool get isEmpty => value.isEmpty;
+  bool get isNotEmpty => value.isNotEmpty;
+
+  int get length => value.length;
+
+  T get first => value.first;
+  T get last => value.last;
+
+  T? get firstOrNull {
+    try {
+      return value.first;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  T? get lastOrNull {
+    try {
+      return value.last;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  void add(T value) {
+    this.value.add(value);
+    refresh();
+  }
+
+  void addAll(Iterable<T> value) {
+    this.value.addAll(value);
+    refresh();
+  }
+
+  bool remove(Object? value) {
+    final removeResult = this.value.remove(value);
+    if (removeResult) {
+      refresh();
+    }
+    return removeResult;
+  }
+
+  void removeWhere(bool Function(T) where) {
+    value.removeWhere(where);
+    refresh();
+  }
+
+  void clear() {
+    value.clear();
+    refresh();
+  }
+
+  Iterable<R> map<R>(R Function(T e) map) => value.map(map);
+
+  Iterable<T> where(bool Function(T) function) => value.where(function);
+  T firstWhere(bool Function(T) function) => value.firstWhere(function);
+  T lastWhere(bool Function(T) function) => value.lastWhere(function);
+}
--- lib/src/variable.dart
+++ lib/src/variable.dart
@@ -5,6 +5,7 @@ import 'package:flutter/foundation.dart';
 part 'extension/on_rv_list.dart';
 part 'extension/on_rv_iterable.dart';
 part 'extension/on_rv_map.dart';
+part 'extension/on_rv_set.dart';
 
 class Rv<T> extends ChangeNotifier implements ValueListenable {
   T _value;
