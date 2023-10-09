bee060c32dccc37ad8ed32264bf1bb4bc8ce4207
Andrey Vakarchuk <dronn1k.dev@gmail.com>
2023-10-09 16:35:22 +0300

    typization has been improved at OnWidget extension.

--- lib/src/extension/on_widget.dart
+++ lib/src/extension/on_widget.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:reactive_variables/reactive_variables.dart';
 
 extension OnWidget on Widget {
-  Obs observe(List<Rv<dynamic>> rvList) =>
+  Obs observe(List<RvInterface> rvList) =>
       Obs(rvList: rvList, builder: (context) => this);
 }
